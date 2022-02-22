b9d5d0584ed442161f58d379409e5e105d7c8ccc
Colin <colinmaisk@gmail.com>
2022-02-21 18:12:10 -0800

    Created controls component

---
+++ components/audioPlayer/audioPlayerControls/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const AudioPlayerControls = (props) => {
+  return (
+    <div className='playerControls playerIcon'>
+    </div>
+  );
+};
+
+export default AudioPlayerControls;
