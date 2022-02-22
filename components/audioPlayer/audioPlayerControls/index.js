22423bee4599f6c062c4bcbf0735c32d68511d1c
Colin <colinmaisk@gmail.com>
2022-02-21 19:47:41 -0800

    created toggle for play button and added play button to audio controls

--- components/audioPlayer/audioPlayerControls/index.js
+++ components/audioPlayer/audioPlayerControls/index.js
@@ -1,11 +1,29 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import PlayButton from './playButton';
 
 const AudioPlayerControls = (props) => {
+  
+  const togglePlayHandler = () => {
+    if (props.isPlaying) {
+    props.onPause();
+    } else {
+    props.onPlay();
+    }
+  };
+
   return (
     <div className='playerControls playerIcon'>
+      <PlayButton togglePlayHandler={ togglePlayHandler } isPlaying={ props.isPlaying } />
     </div>
   );
+};
+
+AudioPlayerControls.PropTypes = {
+    onPause: PropTypes.func.isRequired,
+    onPlay: PropTypes.func.isRequired,
+    isPlaying: PropTypes.bool.isRequired,
+    currentTrack: PropTypes.object.isRequired,
 };
 
 export default AudioPlayerControls;
